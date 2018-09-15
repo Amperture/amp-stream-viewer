ab43032e401a948cb47c440ce851142e6191d6a6
W. Alex Best <amp@amperture.com>
2018-09-15 03:18:36 -0500

    Starts of routes

---
+++ src/components/Home.vue
@@ -0,0 +1,48 @@
+<template>
+<div>  
+<AppHeader/>
+  <section class="hero is-primary">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h2 class="title">Search For YouTube Live Streams</h2>
+        <input class="input has-text-centered" type="text" placeholder="What do you want to search for?"/></input>
+      </div>
+    </div>
+  </section>
+</div>  
+</template>
+
+<script>
+import { mapState } from 'vuex'  
+import Header from './Header'
+
+export default {  
+  components: {
+    'AppHeader': Header
+  },
+  computed: mapState({
+    surveys: state => state.surveys
+  }),
+  beforeMount() {
+    this.$store.dispatch('loadUserInfo')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <section class="hero is-primary is-large has-text-centered">
+    <h1 class="title">Welcome to Amp's YouTube Stream Viewer</h1>
+    <h3 class="subtitle">Please Login to Get Started</h3>
+  </section>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
