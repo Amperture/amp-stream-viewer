5c56946ade8fe9dbba8752a4e5bee266dd2953a0
W. Alex Best <amp@amperture.com>
2018-12-07 05:28:26 -0600

    Beginnings of SearchYoutube page, SearchBar component comitted.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,67 @@
+<template>  <!--{{{-->
+  <div>
+    <form @submit.prevent='handleSearch'>
+      <div class='field is-horizontal'>
+        <!-- Search Bar {{{ -->
+        <div class='control is-expanded has-icons-right'>
+          <input class='input' type='text' v-model='searchText' placeholder='Search...'>
+          <span class='icon is-small is-right'>
+            <font-awesome-icon icon='search'/>
+          </span>
+        </div> 
+        <!-- Search Bar }}} -->
+        <!-- Sort Order {{{ -->
+        <div class='control is-fullwidth is-horizontal'>
+          <div class='select'>
+            <select v-model='selectedOrder'>
+              <option 
+                    v-for="order in orders"
+                    v-bind:value='order.value'>
+                {{ order.text }}
+              </option>
+            </select>
+          </div>
+        </div>
+        <!-- Sort Order }}} -->
+      </div>
+    </form>
+  </div>
+</template> <!--}}}-->
+<script> /* {{{ */
+export default {
+  data(){ // {{{
+    return {
+      selectedOrder: 'relevance',
+      orders: [
+        { 'text' : 'Date', 'value' : 'date' },
+        { 'text' : 'Rating', 'value' : 'rating' },
+        { 'text' : 'Relevance', 'value' : 'relevance' },
+        { 'text' : 'Alphabetically by Title', 'value' : 'title' },
+        { 'text' : 'Number of Viewers', 'value' : 'viewCount' }
+      ],
+      searchText: ''
+    }
+  }, // }}}
+  methods: {//{{{
+    handleSearch(){
+      console.log(this.searchText, this.selectedOrder)
+      this.$router.push({
+        name: 'SearchYoutube',
+        params: {
+          searchTerm: this.searchText,
+          sortOrder:  this.selectedOrder
+        },
+      })
+      /*
+      this.$store.dispatch('searchYoutube', this.search_text)
+        .then(response => {
+        console.log("Search Executed!")
+        })
+      */
+    }
+
+  }, //}}}
+}
+</script> /* }}} */
+<style> /* {{{ */
+</style> /* }}} */
---
+++ src/components/SearchYoutube.vue
@@ -0,0 +1,22 @@
+<template>  <!--{{{-->
+<div>
+<AppHeader/>
+  Hello World {{ this.$route.params.sortOrder }} {{ this.$route.params.searchTerm }}.
+</div>
+</template> <!--}}}-->
+<script> /* {{{ */
+import Header from './Header'
+
+export default {
+  components: { // {{{
+    'AppHeader': Header
+  }, // }}}
+
+  beforeMount(){
+    console.log("Am I mounted yet?")
+    console.log(this.$route.params.sortOrder)
+    console.log(this.$route.params.searchTerm)
+  }
+}
+</script> /* }}} */
+<style>
