d5c6dad590bd7a105ccd842f1527a78bec21337d
W. Alex Best <amp@amperture.com>
2019-01-16 19:53:50 -0600

    Flake8 compliant youtube.py

--- app/youtube.py
+++ app/youtube.py
@@ -1,52 +1,49 @@
-# Python and Flask Imports {{{
 from app import app, db
-from flask import redirect, jsonify, request
+from flask import jsonify, request
 
 from app.util import auth_required
 
-from app.models import User, OAuthCreds, StreamLog, ChatterLog, MessageLog, \
-        Broadcaster, chatters_in_stream
+from app.models import StreamLog, Broadcaster
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 import json
-# }}} 
 
-COMMON_ERRORS = { # {{{
-        'backendError'      : "yt_backend_error",
-        'liveChatEnded'     : 'chat_ended',
-        'liveChatNotFound'  : 'chat_not_found',
-        'liveChatDisabled'  : 'chat_disabled',
+COMMON_ERRORS = {
+        'backendError': "yt_backend_error",
+        'liveChatEnded': 'chat_ended',
+        'liveChatNotFound': 'chat_not_found',
+        'liveChatDisabled': 'chat_disabled',
         }
-# }}}
 
-@app.route('/api/youtube/search', methods=["GET"]) #{{{
+
+@app.route('/api/youtube/search', methods=["GET"])
 @auth_required
 def youtube_search_get(user):
     '''
     Search youtube live streams.
     '''
-    # Process form {{{
+    # Process form
 
     try:
-        form        = request.args
+        form = request.args
+
         # Required Arguemnts
         sortMethod = form['sortMethod']
+
         # Optional Arguments
         searchText = form.get('searchText')
         searchMethod = form.get('searchMethod', '')
-    except:
+    except Exception:
         return jsonify({
             'error': "empty_request"
-            }), 500 
+            }), 500
 
-    # }}}
-    # Sometimes a user will just want to repeat their last search {{{
+    # Sometimes a user will just want to repeat their last search
     if (searchMethod == 'lastSearch'):
         searchText = user['user'].last_search
-    # }}}
-    # {{{ Create Youtube stream search API and execute
+    # Create Youtube stream search API and execute
 
     try:
         youtube = build('youtube', 'v3', credentials=user['credentials'])
@@ -66,47 +63,45 @@ def youtube_search_get(user):
         errorDetail = contentJSON['error']['errors'][0]['reason']
         if errorDetail in COMMON_ERRORS.keys():
             return jsonify({
-                'error' : COMMON_ERRORS[errorDetail]
+                'error': COMMON_ERRORS[errorDetail]
                 }), ex.resp.status
         else:
             print(ex)
             return jsonify({
-                'error' : 'unknown_error'
+                'error': 'unknown_error'
                 }), 500
-    #}}}
 
     response = {
-            'searchResult'     :   searchResult,
-            'jwt'              :   user['jwt']
+            'searchResult': searchResult,
+            'jwt': user['jwt']
             }
 
     return jsonify(response)
 
-#}}} 
-@app.route('/api/youtube/stream', methods=["GET"]) #{{{
+
+@app.route('/api/youtube/stream', methods=["GET"])
 @auth_required
 def youtube_stream_get(user):
     response = {}
-    # Process form {{{
+    # Process form
     try:
         videoID = request.args['videoID']
     except KeyError:
         return jsonify({
             'error': "empty_request"
-            }), 500 
-    # }}}
-    # {{{ Check database for stream info, pull from YT API if not found.
+            }), 500
 
-    stream = StreamLog.query.filter_by(video_id = videoID).first()
-    if not stream: 
-        # {{{ Make API call and grab Chat ID
+    # Check database for stream info, pull from YT API if not found.
+    stream = StreamLog.query.filter_by(video_id=videoID).first()
+    if not stream:
+        # Make API call and grab Chat ID
         try:
             youtube = build('youtube', 'v3', credentials=user['credentials'])
             broadcast = youtube.videos().list(
                     part='id,liveStreamingDetails,snippet',
                     id=videoID
             ).execute()
-            #pprint.pprint(broadcast['items'][0]['snippet'])
+            # pprint.pprint(broadcast['items'][0]['snippet'])
             broadcastInfo = broadcast['items'][0]
             stream, broadcaster = _processBroadcastInfo(broadcastInfo)
 
@@ -115,93 +110,83 @@ def youtube_stream_get(user):
             errorDetail = contentJSON['error']['errors'][0]['reason']
             if errorDetail in COMMON_ERRORS.keys():
                 return jsonify({
-                    'error' : COMMON_ERRORS[errorDetail]
+                    'error': COMMON_ERRORS[errorDetail]
                     }), ex.resp.status
             else:
                 print(ex)
                 return jsonify({
-                    'error' : 'unknown_error'
+                    'error': 'unknown_error'
                     }), 500
 
-        except ValueError as ex: 
+        except ValueError as ex:
             errorDetail = str(ex)
             return jsonify({
-                'error' : COMMON_ERRORS[errorDetail]
+                'error': COMMON_ERRORS[errorDetail]
                 }), 403
-        # }}}
     else:
         broadcaster = stream.streamer
 
-    chatID =  stream.chat_id
+    chatID = stream.chat_id
     streamerName = broadcaster.channel_name
     streamTitle = stream.video_title
     streamDescription = stream.video_description
 
-    # }}}
-
     response = {
-            'chatID'            : chatID,
-            'streamerName'      : streamerName,
-            'streamTitle'       : streamTitle,
-            'streamDescription' : streamDescription,
-            'jwt'               : user['jwt']
+            'chatID': chatID,
+            'streamerName': streamerName,
+            'streamTitle': streamTitle,
+            'streamDescription': streamDescription,
+            'jwt': user['jwt']
             }
 
     return jsonify(response)
 
-#}}} 
 
-def _processBroadcastInfo(broadcastInfo):#{{{
-    '''
+def _processBroadcastInfo(broadcastInfo):
+    """
     Take in the broadcast information returned from YouTube, check if logs
     exist in database of broadcast, broadcaster, and update database tables
     if necessary.
-    '''
-    # Broadcast may be an ended stream, throw error if so {{{
+    """
+    # Broadcast may be an ended stream, throw error if so
     if 'actualEndTime' in broadcastInfo['liveStreamingDetails']:
         raise ValueError('liveChatEnded')
-    # }}} 
 
-
-    # Retrieve broadcaster info from database. Create if vacant. {{{
+    # Retrieve broadcaster info from database. Create if vacant.
 
     '''
     Theoretically these two DB operations could be more efficient as upserts.
     This is worth looking into.
     '''
     broadcaster = Broadcaster.query.filter_by(
-            channel_id = broadcastInfo['snippet']['channelId']).first()
+            channel_id=broadcastInfo['snippet']['channelId']).first()
     if not broadcaster:
         broadcaster = Broadcaster(
-                channel_id = broadcastInfo['snippet']['channelId'],
-                channel_name = broadcastInfo['snippet']['channelTitle']
+                channel_id=broadcastInfo['snippet']['channelId'],
+                channel_name=broadcastInfo['snippet']['channelTitle']
         )
-        try: 
+        try:
             db.session.add(broadcaster)
             db.session.commit()
         except Exception as ex:
             print(ex)
             db.session.rollback()
-    # }}}
-    # Retrieve stream info from database. Create if vacant. # {{{
+
+    # Retrieve stream info from database. Create if vacant.
     stream = StreamLog.query.filter_by(
-            video_id = broadcastInfo['id']).first()
+            video_id=broadcastInfo['id']).first()
     if not stream:
         stream = StreamLog(
-                video_id = broadcastInfo['id'],
-                video_title = broadcastInfo['snippet']['title'],
-                video_description = broadcastInfo['snippet']['description'],
-                streamer_id = broadcaster.channel_id,
-                chat_id = 
-                    broadcastInfo['liveStreamingDetails']['activeLiveChatId']
+            video_id=broadcastInfo['id'],
+            video_title=broadcastInfo['snippet']['title'],
+            video_description=broadcastInfo['snippet']['description'],
+            streamer_id=broadcaster.channel_id,
+            chat_id=broadcastInfo['liveStreamingDetails']['activeLiveChatId']
         )
-        try: 
+        try:
             db.session.add(stream)
             db.session.commit()
         except Exception as ex:
             print(ex)
             db.session.rollback()
-    #}}}
     return stream, broadcaster
-
-#}}}
